7a21d9b502bce41c1bd3383e7613017b675dec52
Justin Shenk <shenkjustin@gmail.com>
2019-01-22 15:19:04 +0100

    Iniital commit

---
+++ traja/__init__.py
@@ -0,0 +1,1 @@
+from .main import traj_from_coords, from_file
--- traja/utils.py
+++ traja/utils.py
@@ -1,4 +1,6 @@
 #! /usr/local/env python3
+import numpy as np
+import scipy
 
 def stylize_axes(ax):
     ax.spines['top'].set_visible(False)
@@ -15,3 +17,55 @@ def shift_xtick_labels(xtick_labels, first_index=None):
         if first_index is not None:
             xtick_labels[0] = first_index
     return xtick_labels
+
+def fill_in_traj(trj):
+
+
+def smooth_sg(trj, w = None, p = 3):
+    """Savitzky-Golay filtering."""
+    if w is None:
+        w = p + 3 - p % 2
+
+    if (w % 2 != 1):
+        raise Exception(f"Invalid smoothing parameter w ({w}): n must be odd")
+    trj.x = scipy.signal.savgol_filter(trj.x, window_length = w, polyorder=p, axis=0)
+    trj.y = scipy.signal.savgol_filter(trj.y, window_length = w, polyorder=p, axis=0)
+    trj = fill_in_traj(trj)
+    return trj
+
+def angles(trj, lag = 1, compass_direction = None):
+    trj['angle'] = np.rad2deg(np.arccos(np.abs(trj['dx']) / trj['distance']))
+    # Get heading from angle
+    mask = (trj['dx'] > 0) & (trj['dy'] >= 0)
+    trj.loc[mask, 'heading'] = trj['angle'][mask]
+    mask = (trj['dx'] >= 0) & (trj['dy'] < 0)
+    trj.loc[mask, 'heading'] = -trj['angle'][mask]
+    mask = (trj['dx'] < 0) & (trj['dy'] <= 0)
+    trj.loc[mask, 'heading'] = -(180 - trj['angle'][mask])
+    mask = (trj['dx'] <= 0) & (trj['dy'] > 0)
+    trj.loc[mask, 'heading'] = (180 - trj['angle'])[mask]
+    trj['turn_angle'] = trj['heading'].diff()
+    # Correction for 360-degree angle range
+    trj.loc[trj.turn_angle >= 180, 'turn_angle'] -= 360
+    trj.loc[trj.turn_angle < -180, 'turn_angle'] += 360
+
+
+def expected_sq_displacement(trj, n = None, eqn1= True, compass_direction = None):
+    sl = step_lengths(trj)
+    ta = angles(trj, compass_direction = compass_direction)
+    l = np.mean(sl)
+    l2 = np.mean(sl ^ 2)
+    c = np.mean(np.cos(ta))
+    s = np.mean(np.sin(ta))
+    s2 = s ^ 2
+
+    if eqn1:
+        # Eqn 1
+        alpha = np.arctan2(s, c)
+        gamma = ((1 - c)^2 - s2) * np.cos((n + 1) * alpha) - 2 * s * (1 - c) * np.sin((n + 1) * alpha)
+        esd = n * l2 + 2 * l^2 * ((c - c^2 - s2) * n  - c) / ((1 - c)^2 + s2) + 2 * l^2 * ((2 * s2 + (c + s2) ^ ((n + 1) / 2)) / ((1 - c)^2 + s2)^2) * gamma
+        return abs(esd)
+    else:
+        # Eqn 2
+        esd = n * l2 + 2 * l ^ 2 * c / (1 - c) * (n - (1 - c ^ n) / (1 - c))
+        return esd
