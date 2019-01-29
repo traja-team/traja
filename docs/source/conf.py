dd9b09f5e1d01932c6c91c3bf698f36feee39d98
Justin Shenk <shenkjustin@gmail.com>
2019-01-29 01:15:39 +0100

    Update theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Justin Shenk'
 # The short X.Y version
 version = ''
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.2-alpha.1'
 
 
 # -- General configuration ---------------------------------------------------
@@ -77,7 +77,7 @@ pygments_style = None
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
