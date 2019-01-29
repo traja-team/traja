3f4a5e0aef90e3b2fbf3a4b77e5be1d6d26b160a
Justin Shenk <shenkjustin@gmail.com>
2019-01-29 17:48:04 +0100

    Comment out unused imports

--- traja/__init__.py
+++ traja/__init__.py
@@ -1,7 +1,7 @@
 from .main import TrajaDataFrame
 from .accessor import TrajaAccessor
 from .utils import generate, from_df, from_xy, traj_from_coords, read_file
-from .contrib import dvc
+# from .contrib import dvc
 
 __author__ = 'justinshenk'
 __version__ = '0.0.2-alpha.2'
