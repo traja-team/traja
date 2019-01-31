8f0669ef3218f1d7e85ff234ee19d14406109ffe
Justin Shenk <shenkjustin@gmail.com>
2019-01-31 19:51:28 +0100

    Update gitignore

--- traja/__init__.py
+++ traja/__init__.py
@@ -4,4 +4,4 @@ from .utils import generate, from_df, from_xy, traj_from_coords, read_file, sans
 # from .contrib import dvc
 
 __author__ = 'justinshenk'
-__version__ = '0.0.2-alpha.3'
+__version__ = '0.0.2-alpha.4'
