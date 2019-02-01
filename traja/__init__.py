625964ea6c098282004d754ee37e0c7a6f2c326a
Justin Shenk <shenkjustin@gmail.com>
2019-02-01 20:03:52 +0100

    Bump version to 0.0.4 for travis build

--- traja/__init__.py
+++ traja/__init__.py
@@ -1,7 +1,7 @@
 from .main import TrajaDataFrame
 from .accessor import TrajaAccessor
-from .utils import generate, from_df, from_xy, traj_from_coords, read_file, sans_serif
+from .utils import *
 # from .contrib import dvc
 
 __author__ = 'justinshenk'
-__version__ = '0.0.3'
+__version__ = '0.0.4'
