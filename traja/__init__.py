f637da19bb95b0bda99a52bc09d5705e96ebe08c
Justin Shenk <shenkjustin@gmail.com>
2019-02-06 19:22:32 +0100

    Fix bug with plotting lstm

--- traja/__init__.py
+++ traja/__init__.py
@@ -2,6 +2,7 @@ from .trajadataframe import TrajaDataFrame
 from .accessor import TrajaAccessor
 from .plotting import *
 from .trajectory import *
+from . import models
 
 __author__ = 'justinshenk'
 __version__ = '0.0.8'
--- traja/models/nn.py
+++ traja/models/nn.py
@@ -38,6 +38,7 @@ class TrajectoryLSTM():
         fig, ax = plt.subplots(2, 1)
         self.fig = fig
         self.ax = ax
+        assert(xy.shape[1] is 2, f"xy should be an N x 2 array, but is {xy.shape}")
         self.xy = xy
         self.nb_steps = nb_steps
         self.epochs = epochs
@@ -46,17 +47,17 @@ class TrajectoryLSTM():
         self.rnn = LSTM()
 
     def load_batch(self, batch_size=32):
-        t_1_b = np.zeros((nb_steps, batch_size, 2))
-        t_b = np.zeros((nb_steps * batch_size, 2))
+        t_1_b = np.zeros((self.nb_steps, self.batch_size, 2))
+        t_b = np.zeros((self.nb_steps * self.batch_size, 2))
 
-        inds = np.random.randint(0, len(self.xy) - nb_steps, (batch_size))
+        inds = np.random.randint(0, len(self.xy) - self.nb_steps, (self.batch_size))
         for i, ind in enumerate(inds):
-            t_1_b[:, i] = self.xy[ind:ind + nb_steps]
-            t_b[i * nb_steps:(i + 1) * nb_steps] = self.xy[ind + 1:ind + nb_steps + 1]
+            t_1_b[:, i] = self.xy[ind:ind + self.nb_steps]
+            t_b[i * nb_steps:(i + 1) * self.nb_steps] = self.xy[ind + 1:ind + nb_steps + 1]
         return torch.from_numpy(t_1_b).float(), torch.from_numpy(t_b).float()
 
     def train(self):
-        mean_loss = 0.
+        self.mean_loss = 0.
         for epoch in range(1, self.epochs + 1):
             t_1_b, t_b = self.load_batch(self.batch_size)
 
@@ -72,11 +73,11 @@ class TrajectoryLSTM():
 
             optimizer = optim.Adam(self.rnn.parameters(), 1e-3)
             optimizer.step(closure)
-            mean_loss += loss.item()
+            self.mean_loss += loss.item()
 
             if epoch % 100 == 0:
-                print('Epoch: {} | Loss: {:.6f}'.format(epoch, mean_loss))
-                mean_loss = 0
+                print('Epoch: {} | Loss: {:.6f}'.format(epoch, self.mean_loss))
+                self.mean_loss = 0
 
     def savefig(self, filepath):
         self.fig.savefig(filepath)
@@ -97,23 +98,17 @@ class TrajectoryLSTM():
         for a in self.ax:
             a.legend()
 
-    def plot(self, interactive=False, as_generator=False):
+    def plot(self, interactive=True):
         if interactive and (plt.get_backend() == 'agg'):
             logging.ERROR("Not able to use interactive plotting in mpl `agg` mode.")
             interactive = False
-        if as_generator:
-            while True:
-                self._plot()
-                yield self.fig
         elif interactive:
             while True:
                 for a in self.ax:
                     a.clear()
                 self._plot()
-                plt.pause(0.1)
-                plt.show()
-                input("Press [enter] to continue.")
-
+                plt.pause(1)
+                plt.show(block=False)
         else:
             self._plot()
             return self.fig
