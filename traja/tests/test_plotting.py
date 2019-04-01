650cb9f7176bff4a35e9a02f3a21d42befb7464b
Justin Shenk <shenkjustin@gmail.com>
2019-04-01 14:42:21 +0200

    Add tests

--- traja/tests/test_plotting.py
+++ traja/tests/test_plotting.py
@@ -5,3 +5,15 @@ from pandas.util.testing import assert_frame_equal
 import traja
 
 df = traja.generate()
+
+
+def test_plot_flow():
+    ...
+
+
+def test_plot_stream():
+    ...
+
+
+def test_trip_grid():
+    ...
