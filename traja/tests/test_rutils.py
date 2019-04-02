fd83f314c7aa4d08aee1ebae2c9bc0ed52c9f45f
Justin Shenk <shenkjustin@gmail.com>
2019-04-02 09:32:52 +0200

    Add test coverage, fix minor bugs

--- traja/tests/test_rutils.py
+++ traja/tests/test_rutils.py
@@ -1,11 +1,11 @@
-import numpy as np
-import numpy.testing as npt
-from pandas.util.testing import assert_frame_equal
+import warnings
 
 import traja
 import traja.rutils
 
-df = traja.generate()
+warnings.filterwarnings("ignore", category=UserWarning, module="rpy2")
+
+df = traja.generate(n=20)
 
 
 def test_import_adehabitat():
@@ -20,23 +20,17 @@ def test_import_trajr():
     trajr = import_trajr()
 
 
-def test_plot_ltraj():
-    ...
-
-
 def test_to_trajr():
     trjr = traja.rutils.to_trajr(df)
     vals = trjr[0]
     actual = vals.r_repr()
-    expected = (
-        "c(0, 1.34184903653994, 2.36458915140914, 2.36329148545331, 0.54325141464563\n)"
-    )
-    assert actual == expected
+    expected = "c(0, 0.946646340454933, 1.94695932892542, 1.94548732303614, 0.277985344978653, \n"
+    assert expected in actual
 
 
 def test_to_ltraj():
     ltraj = traja.rutils.to_ltraj(df)
     vals = ltraj[0][0]
-    actual = vals.r_repr()[:20]
-    expected = "c(0, 1.34184903653994, 2.36458915140914,"
+    actual = vals.r_repr()[:24]
+    expected = "c(0, 0.946646340454933, "
     assert actual == expected
