f2429a23779ae3d8508c175c31d95440caa4f103
Justin Shenk <shenkjustin@gmail.com>
2019-07-24 15:17:24 +0200

    Fix colorbar labels for time intervals

--- traja/plotting.py
+++ traja/plotting.py
@@ -267,7 +267,8 @@ def plot(
         )
     else:
         # Convert frames to time
-        cbar_labels = trj.index[indices].values
+        cbar_labels = trj[time_col][indices].values
+        cbar_labels = np.round(cbar_labels, 6)
         if fps is not None and fps > 0 and fps is not 1 and show_time:
             cbar_labels = cbar_labels / fps
 
