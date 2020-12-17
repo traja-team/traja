e1cc8bd5cb4c057434881bca162789455fc2038a
Saran-nns <sarannsworld@gmail.com>
2020-12-17 18:54:37 +0100

    lstm optim fix

--- traja/models/losses.py
+++ traja/models/losses.py
@@ -1,46 +1,27 @@
 import torch
 
 
-class Criterion(object):
+class Criterion:
 
-    def __init__(self, model_type):
-        self.model_type = model_type
+    def __init__(self):
         self.huber_loss = torch.nn.SmoothL1Loss(reduction='sum')
         self.crossentropy_loss = torch.nn.CrossEntropyLoss()
 
-    def ae_criterion(self, recon_x, x, loss_type='huber'):
-        """[summary]
+    def ae_criterion(self, predicted, target, loss_type='huber'):
 
-        Args:
-            recon_x ([type]): [description]
-            x ([type]): [description]
-            loss_type(str): Type of Loss; huber or RMSE
-
-        Returns:
-            [type]: [description]
-        """
         if loss_type == 'huber':
-            dist_x = self.huber_loss(recon_x, x)
-            return dist_x
+            loss = self.huber_loss(predicted, target)
+            return loss
         else:  # Root MSE
-            return torch.sqrt(torch.mean((recon_x - x) ** 2))
+            return torch.sqrt(torch.mean((predicted - target) ** 2))
 
-    def vae_criterion(self, recon_x, x, mu, logvar, loss_type='huber'):
+    def vae_criterion(self, predicted, target, mu, logvar, loss_type='huber'):
         """Time series generative model loss function
-
-        Args:
-            recon_x ([type]): [description]
-            x ([type]): [description]
-            mu ([type]): [description]
-            logvar ([type]): [description]
-
-        Returns:
-            [type]: [description]
         """
         if loss_type == 'huber':
-            dist_x = self.huber_loss(recon_x, x)
+            dist_x = self.huber_loss(predicted, target)
         else:
-            dist_x = torch.sqrt(torch.mean((recon_x - x) ** 2))
+            dist_x = torch.sqrt(torch.mean((predicted - target) ** 2))
         KLD = -0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp())
         return dist_x + KLD
 
