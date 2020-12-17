ba939b8e4a3b84c8fc29ad38486bf2147bbe8a3e
Saran-nns <sarannsworld@gmail.com>
2020-12-17 12:49:45 +0100

    lstm fix

--- traja/models/losses.py
+++ traja/models/losses.py
@@ -5,9 +5,10 @@ class Criterion(object):
 
     def __init__(self, model_type):
         self.model_type = model_type
+        self.huber_loss = torch.nn.SmoothL1Loss(reduction='sum')
+        self.crossentropy_loss = torch.nn.CrossEntropyLoss()
 
-    @staticmethod
-    def ae_criterion(recon_x, x, loss_type='huber'):
+    def ae_criterion(self, recon_x, x, loss_type='huber'):
         """[summary]
 
         Args:
@@ -19,15 +20,12 @@ class Criterion(object):
             [type]: [description]
         """
         if loss_type == 'huber':
-
-            huber_loss = torch.nn.SmoothL1Loss(reduction='sum')
-            dist_x = huber_loss(recon_x, x)
+            dist_x = self.huber_loss(recon_x, x)
             return dist_x
-        else:  # RMSE
+        else:  # Root MSE
             return torch.sqrt(torch.mean((recon_x - x) ** 2))
 
-    @staticmethod
-    def vae_criterion(recon_x, x, mu, logvar, loss_type='huber'):
+    def vae_criterion(self, recon_x, x, mu, logvar, loss_type='huber'):
         """Time series generative model loss function
 
         Args:
@@ -40,26 +38,23 @@ class Criterion(object):
             [type]: [description]
         """
         if loss_type == 'huber':
-            huber_loss = torch.nn.SmoothL1Loss(reduction='sum')
-            dist_x = huber_loss(recon_x, x)
+            dist_x = self.huber_loss(recon_x, x)
         else:
             dist_x = torch.sqrt(torch.mean((recon_x - x) ** 2))
-
         KLD = -0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp())
-
         return dist_x + KLD
 
-    @staticmethod
-    def classifier_criterion(predicted,target):
+    def classifier_criterion(self, predicted, target):
         """Classifier loss function"""
-        loss = torch.nn.CrossEntropyLoss()
-        loss = loss(predicted, target)
+        loss = self.crossentropy_loss(predicted, target)
+        return loss
+
+    def lstm_criterion(self, predicted, target):
+
+        loss = self.huber_loss(predicted, target)
         return loss
 
     def vaegan_criterion(self):
-        return NotImplementedError
-
-    def lstm_criterion(self):
         return NotImplementedError
 
 # VAE loss
