761e753d3c04921933efad3a70024b9130fc8e90
Saran-nns <saran_nns@hotmail.com>
2020-12-20 17:38:02 +0100

    update classifier hybrid networks

--- traja/models/optimizers.py
+++ traja/models/optimizers.py
@@ -5,13 +5,14 @@ from traja.models.ae import MultiModelAE
 
 class Optimizer:
 
-    def __init__(self, model_type, model, optimizer_type):
+    def __init__(self, model_type, model, optimizer_type, classify=False):
         """
         Wrapper for setting the model optimizer and learning rate schedulers using ReduceLROnPlateau;
         If the model type is 'ae' or 'vae' - var optimizers is a dict with separate optimizers for encoder, decoder,
         latent and classifier. In case of 'lstm', var optimizers is an optimizer for lstm and TimeDistributed(linear layer)
         :param model_type: Type of model 'ae', 'vae' or 'lstm'
         :param model: Model instance
+        :param classify: If True, will return the Optimizer and scheduler for classifier
         :param optimizer_type: Optimizer to be used; Should be one in ['Adam', 'Adadelta', 'Adagrad', 'AdamW', 'SparseAdam', 'RMSprop', 'Rprop',
                                        'LBFGS', 'ASGD', 'Adamax']
         """
@@ -23,6 +24,7 @@ class Optimizer:
         self.model_type = model_type
         self.model = model
         self.optimizer_type = optimizer_type
+        self.classify = classify
         self.optimizers = {}
         self.schedulers = {}
 
@@ -46,9 +48,12 @@ class Optimizer:
             for network in keys:
                 self.optimizers[network] = getattr(torch.optim, f'{self.optimizer_type}')(
                     getattr(self.model, f'{network}').parameters(), lr=lr)
+            if not self.classify:
+                self.optimizers['classifier'] = None
 
         elif self.model_type == 'vaegan':
             return NotImplementedError
+
         return self.optimizers
 
     def get_lrschedulers(self, factor: float, patience: int):
@@ -69,8 +74,10 @@ class Optimizer:
                                                 patience=patience, verbose=True)
         else:
             for network in self.optimizers.keys():
-                self.schedulers[network] = ReduceLROnPlateau(self.optimizers[network], mode='max', factor=factor,
-                                                             patience=patience, verbose=True)
+                if self.optimizers[network] is not None:
+                    self.schedulers[network] = ReduceLROnPlateau(self.optimizers[network], mode='max', factor=factor,
+            if not self.classify:                                                     patience=patience, verbose=True)
+                self.schedulers['classifier'] = None
         return self.schedulers
 
 
