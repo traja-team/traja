5e1ff12dfd7d3c6296442de31d1a55faaf3541dd
Saran-nns <sarannsworld@gmail.com>
2020-12-21 13:49:44 +0100

    Update train.py

--- traja/models/train.py
+++ traja/models/train.py
@@ -202,8 +202,7 @@ class HybridTrainer(object):
 
             if epoch + 1 == self.epochs:
                 training_mode = 'classification'
-                print('Training classifier')
-
+                
             # Testing
             if epoch % 10 == 0:
                 with torch.no_grad():
