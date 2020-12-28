bf1ffa79fb4b496cc8444375ee2012f3572abff4
Saran-nns <sarannsworld@gmail.com>
2020-12-28 15:27:32 +0100

    Update optimizers.py

--- traja/models/optimizers.py
+++ traja/models/optimizers.py
@@ -1,6 +1,10 @@
 import torch
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from traja.models.ae import MultiModelAE
+from traja.models.generative_models.vae import MultiModelVAE
+from traja.models.predictive_models.ae import MultiModelAE
+from traja.models.predictive_models.lstm import LSTM
+from traja.models.predictive_models.irl import MultiModelIRL
+from traja.models.generative_models.vaegan import MultiModelVAEGAN
 
 
 class Optimizer:
