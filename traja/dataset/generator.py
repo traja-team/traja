6552d088f3dec2deca12bd5cdcd056b2e5dbd7bc
Wolf Byttner <wolf@rapidhealth.co.uk>
2021-01-10 10:49:19 +0000

    Make category-wise sampling faster

--- traja/dataset/generator.py
+++ traja/dataset/generator.py
@@ -47,6 +47,8 @@ def generate_dataset(df, n_past: int, n_future: int, stride: int = None, paramet
     assert n_future >= 1, 'n_past has to be positive!'
     assert stride >= 1, 'Stride has to be positive!'
 
+    sequences_in_categories= list()
+
     for ID in series_ids.keys():
         xx, yy, zz, ww = list(), list(), list(), list()
         # Drop the column ids and convert the pandas into arrays
@@ -54,6 +56,7 @@ def generate_dataset(df, n_past: int, n_future: int, stride: int = None, paramet
         series = series_ids[ID].drop(columns=['ID'] + parameter_columns).to_numpy()
         parameters = series_ids[ID].drop(columns=non_parameter_columns).to_numpy()[0, :]
         window_start = 0
+        sequences_in_category = 0
         while window_start <= len(series):
             past_end = window_start + n_past
             future_end = past_end + n_future
@@ -66,13 +69,32 @@ def generate_dataset(df, n_past: int, n_future: int, stride: int = None, paramet
                 # For each sequence length set target category
                 zz.append(int(ID), )
                 ww.append(parameters)
+                sequences_in_category += 1
             window_start += stride
 
         train_data.extend(np.array(xx))
         target_data.extend(np.array(yy))
         target_category.extend(np.array(zz))
         target_parameters.extend(np.array(ww))
-    return train_data, target_data, target_category, target_parameters
+        sequences_in_categories.append(sequences_in_category)
+    return train_data, target_data, target_category, target_parameters, sequences_in_categories
+
+
+def get_indices_from_categories(categories: list, sequences_in_categories: list):
+    indices = list()
+    sequence_index = 0
+    start_index = 0
+    for category in categories:
+        while sequence_index < len(sequences_in_categories) and sequence_index < category:
+            start_index += sequences_in_categories[sequence_index]
+            sequence_index += 1
+        if sequence_index >= len(sequences_in_categories):
+            break
+        if sequence_index == category:
+            indices += list(range(start_index, start_index + sequences_in_categories[sequence_index]))
+        start_index += sequences_in_categories[sequence_index]
+        sequence_index += 1
+    return indices
 
 
 def shuffle_split(
