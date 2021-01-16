da1d9cac35b6a490c6f1ee26d2142f9a8e05a89b
Wolf Byttner <wolf@rapidhealth.co.uk>
2021-01-16 16:15:49 +0000

    Optimise __init__ files

--- traja/models/__init__.py
+++ traja/models/__init__.py
@@ -1,4 +1,3 @@
-# from .nn import LSTM
 from traja.models.generative_models.vae import MultiModelVAE
 from traja.models.generative_models.vaegan import MultiModelVAEGAN
 from traja.models.predictive_models.ae import MultiModelAE
@@ -6,3 +5,4 @@ from traja.models.predictive_models.irl import MultiModelIRL
 from traja.models.predictive_models.lstm import LSTM
 from .inference import *
 from .utils import TimeDistributed, read_hyperparameters, save, load
+from .train import HybridTrainer
