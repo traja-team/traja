1e2f8caaa2140f6d6be2419100878aeb0a4d35a2
Wolf Byttner <wolf@rapidhealth.co.uk>
2021-02-18 10:58:59 +0000

    Add dt to Brownian

--- traja/tests/test_stats.py
+++ traja/tests/test_stats.py
@@ -42,3 +42,32 @@ def test_brownians_with_different_variances_drift_approximately_equally():
     drift2 /= length
 
     np.testing.assert_approx_equal(drift1, drift2, significant=3)
+
+
+def test_brownians_with_different_time_steps_walk_approximately_equally():
+    mean_drift = 0.23
+    variance = 0.52
+
+    time_step_ratio = 7
+    dt1 = 0.1
+    dt2 = dt1 * time_step_ratio
+
+    length2 = 1000000
+    length1 = length2 * time_step_ratio
+
+    drift1 = 0
+    drift2 = 0
+
+    brownian1 = Brownian(length=length1, mean_value=mean_drift, variance=variance, dt=dt1)
+    brownian2 = Brownian(length=length2, mean_value=mean_drift, variance=variance, dt=dt2)
+
+    for i in range(length1):
+        drift1 = brownian1()
+
+    for i in range(length2):
+        drift2 = brownian2()
+
+    drift1 /= length1
+    drift2 /= length2
+
+    np.testing.assert_approx_equal(drift1, drift2, significant=3)
