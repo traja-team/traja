0d312d4d41091c450135a7d8ea00bbbe082ae66d
Justin Shenk <shenkjustin@gmail.com>
2021-06-04 11:52:32 -0400

    [Docs] Update plotting and index

--- docs/examples/plot_3d.py
+++ docs/examples/plot_3d.py
@@ -5,21 +5,15 @@ Plot trajectories with time in the vertical axis.
 Note: Adjust matplotlib args ``dist``, ``labelpad``, ``aspect`` and ``adjustable```
 as needed.
 """
+
 import traja
 
 df = traja.TrajaDataFrame({"x": [0, 1, 2, 3, 4], "y": [1, 3, 2, 4, 5]})
-
 trj = traja.generate(seed=0)
-ax = trj.traja.plot_3d(dist=15, labelpad=32, title="Traja 3D Plot")
 
-########
-# Colors
-# -------
-#
-# `Matplotlib cmaps<https://matplotlib.org/examples/color/colormaps_reference.html>`_ are available
+###############################################################################
+# Plot a trajectory in 3D
+# =======================
+# A 3D plot is generated using :func:`~traja.plotting.plot_3d`.
 
-trj.traja.plot_3d(dist=15, labelpad=32, title="Traja 3D Plot", cmap="jet")
-
-.. video:: https://github.com/traja-team/traja/blob/master/docs/_static/trajectory.mp4
-   :autoplay:
-   :nocontrols:
+trj.traja.plot_3d(dist=15, labelpad=32, title="Traja 3D Plot")
--- docs/examples/plot_animate.py
+++ docs/examples/plot_animate.py
@@ -5,11 +5,12 @@ traja allows animating trajectories.
 """
 import traja
 
-df = traja.generate(100, seed=0)
+df = traja.generate(1000, seed=0)
 
 ###############################################################################
 # Plot a animation of trajectory
 # ==============================
 # An animation is generated using :func:`~traja.plotting.animate`.
 
-traja.plotting.animate(df, save=True) # saves to trajectory.mp4
+anim = traja.plotting.animate(df, save=True) # saves to trajectory.mp4
+anim
