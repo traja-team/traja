81600aa622e245069685bbe365e8bcb5a7ef60a4
Wolf Byttner <wolf@rapidhealth.co.uk>
2021-06-13 11:42:22 +0100

    Started re-drafting pituitary gland with classes

--- traja/dataset/pituitary_gland.py
+++ traja/dataset/pituitary_gland.py
@@ -4,12 +4,13 @@ from numpy import exp
 from numba import jit
 from scipy.integrate import odeint
 from pyDOE2 import lhs
+import peakutils
 
 
 # PyTest will not compute coverage correctly for @jit-compiled code.
 # Thus we must explicitly suppress the coverage check.
 @jit
-def pituitary_ode(w, t, p):  # pragma: no cover
+def pituitary_ode_fletcher(w, t, p):  # pragma: no cover
     """
     Defines the differential equations for the pituirary gland system.
     To be used with scipy.integrate.odeint (this is the rhs equation).
@@ -70,10 +71,10 @@ def pituitary_ode(w, t, p):  # pragma: no cover
     return dv, dn, df, dc
 
 
-def compute_pituitary_gland_df_from_parameters(downsample_rate,
-                                               gcal, gsk, gk, gbk, gl, kc,
-                                               sample_id,
-                                               trim_start=20000):
+def compute_pituitary_gland_df_fletcher_from_parameters(downsample_rate,
+                                                        gcal, gsk, gk, gbk, gl, kc,
+                                                        sample_id,
+                                                        trim_start=20000):
     """
     Computes a Traja dataframe from the pituitary gland simulation.
 
@@ -114,7 +115,7 @@ def compute_pituitary_gland_df_from_parameters(downsample_rate,
 
     t = np.arange(0, 5000, 0.05)
     # print("Generating gcal={}, gsk={}, gk={}, gbk={}, gl={}, kc={}".format(gcal, gsk, gk, gbk, gl, kc))
-    wsol = odeint(pituitary_ode, w0, t, args=(p,), atol=abserr, rtol=relerr)
+    wsol = odeint(pituitary_ode_fletcher, w0, t, args=(p,), atol=abserr, rtol=relerr)
     df = pd.DataFrame(wsol, columns=['v', 'n', 'f', 'c'])
     df = df[trim_start:]
     df['ID'] = sample_id
@@ -142,10 +143,431 @@ def create_latin_hypercube_sampled_pituitary_df(downsample_rate=100, samples=100
     dataframes = []
     for sample_id, parameter in enumerate(parameters):
         gcal, gsk, gk, gbk, gl, kc = parameter
-        df = compute_pituitary_gland_df_from_parameters(downsample_rate,
-                                                        gcal, gsk, gk, gbk, gl, kc,
-                                                        sample_id)
+        df = compute_pituitary_gland_df_fletcher_from_parameters(downsample_rate,
+                                                                 gcal, gsk, gk, gbk, gl, kc,
+                                                                 sample_id)
         dataframes.append(df)
 
     num_samples = len(dataframes)
     return pd.concat(dataframes), num_samples
+
+
+def uniform_scale(val_min, val_mean, val_max):
+    return np.random.uniform(val_min / abs(val_mean), val_max / abs(val_mean))
+
+
+@jit
+def x_inf(V, V_x, s_x):
+    return 1 / (1 + exp((V_x - V) / s_x))
+
+
+@jit
+def s_inf(c, k_s):
+    return c ** 2 / (c ** 2 + k_s ** 2)
+
+
+@jit
+def pituitary_ode(w, t, p):  # pragma: no cover
+    """
+    Defines the differential equations for the pituirary gland system.
+    To be used with scipy.integrate.odeint (this is the rhs equation).
+
+    Arguments:
+        w :  vector of the state variables:
+                  w = [v, n, f, c]
+        t :  time
+        p :  vector of the parameters:
+                  p = [gk, gcal, gsk, gbk, gl, kc]
+    """
+    V, n, m, b, h, h_T, h_Na, c = w
+
+    (g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak,
+    tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c) = p
+
+    E_Ca = 60
+    E_K = -75
+    E_leak = -50 * E_leak_scale  # (-75 - -10)
+    E_Na = 75
+
+    V_m = -20
+    V_mt = -38
+    V_ht = -56
+    V_n = -5
+    V_k = -65
+    V_b = -20
+    V_a = -20
+    V_h = -60
+    V_mNa = -15
+    V_hNa = -60
+
+    C_m = 10 * C_m_scale
+
+    s_m = 12
+    s_mt = 6
+    s_ht = -5
+    s_n = 10
+    s_k = -8
+    s_b = 2
+    s_a = 10
+    s_h = -5
+    s_mNa = 5
+    s_hNa = -10
+
+    f_c = 0.01
+    alpha = 0.0015
+    k_s = 0.4  # (0.06, 0.21)
+
+    I_CaL = g_CaL * m * (V - E_Ca)
+    I_CaT = g_CaT * x_inf(V, V_mt, s_mt) * h_T * (V - E_Ca)
+    I_K = g_K * n * (V - E_K)
+    I_SK = g_SK * s_inf(c, k_s) * (V - E_K)
+    I_Kir = g_Kir * x_inf(V, V_k, s_k) * (V - E_K)
+    I_BK = g_BK * b * (V - E_K)
+    I_NaV = g_NaV * pow(x_inf(V, V_mNa, s_mNa), 3) * h_Na * (V - E_Na)
+    I_A = g_A * x_inf(V, V_a, s_a) * h * (V - E_K)
+    I_leak = g_leak * (V - E_leak)
+
+    I = I_CaL + I_CaT + I_K + I_SK + I_Kir + I_BK + I_NaV + I_A + I_leak
+
+    dv = -I / C_m
+
+    dn = (x_inf(V, V_n, s_n) - n) / tau_n
+    dm = (x_inf(V, V_m, s_m) - m) / tau_m
+    db = (x_inf(V, V_b, s_b) - b) / tau_BK
+    dh = (x_inf(V, V_h, s_h) - h) / tau_h
+    dh_T = (x_inf(V, V_ht, s_ht) - h_T) / tau_ht
+    dh_Na = (x_inf(V, V_hNa, s_hNa) - h_Na) / tau_hNa
+
+    dc = -f_c * (alpha * I_CaL + k_c * c)
+
+    return dv, dn, dm, db, dh, dh_T, dh_Na, dc
+
+
+def pituitary_ori_ode_parameters():
+    # Maximal conductance
+    g_CaL = np.random.uniform(0, 4)
+    g_CaT = 0
+    g_K = np.random.uniform(0., 10.)
+    g_SK = 0
+    g_Kir = 0
+    g_BK = 0
+    g_NaV = 0
+    g_A = 0
+    g_leak = np.random.uniform(0.05, 0.4)
+
+    # Other structural parameters
+    C_m = 1
+    E_leak = 1
+
+    # Kinetic variables
+    tau_m = 1
+    tau_ht = 1
+    tau_n = 1
+    tau_BK = 1
+    tau_h = 1
+    tau_hNa = 1
+    k_c = np.random.uniform(0.03, 0.21)
+
+    return g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak, \
+        tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c
+
+
+def pituitary_ori_ode_parameters_Isk():
+    # Maximal conductance
+    g_CaL = np.random.uniform(0, 4)
+    g_CaT = 0
+    g_K = np.random.uniform(0., 10.)
+    g_SK = np.random.uniform(.5, 3.5)
+    g_Kir = 0
+    g_BK = 0
+    g_NaV = 0
+    g_A = 0
+    g_leak = np.random.uniform(0.05, 0.4)
+
+    # Other structural parameters
+    C_m = np.random.uniform(4, 12)
+    E_leak = 1
+
+    # Kinetic variables
+    tau_m = 1
+    tau_ht = 1
+    tau_n = 1
+    tau_BK = 1
+    tau_h = 1
+    tau_hNa = 1
+    k_c = np.random.uniform(0.03, 0.21)
+
+    return g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak, \
+        tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c
+
+
+def pituitary_ori_ode_parameters_Isk_Ibk():
+    # Maximal conductance
+    g_CaL = np.random.uniform(0, 4)
+    g_CaT = 0
+    g_K = np.random.uniform(0., 10.)
+    g_SK = np.random.uniform(.5, 3.5)
+    g_Kir = 0
+    g_BK = np.random.uniform(0, 4)
+    g_NaV = 0
+    g_A = 0
+    g_leak = np.random.uniform(0.05, 0.4)
+
+    # Other structural parameters
+    C_m = np.random.uniform(4, 12)
+    E_leak = 1
+
+    # Kinetic variables
+    tau_m = np.random.uniform(.7, 1.3)
+    tau_ht = 1
+    tau_n = np.random.uniform(20, 40)
+    tau_BK = np.random.uniform(2, 10)
+    tau_h = 1
+    tau_hNa = 1
+    k_c = np.random.uniform(0.03, 0.21)
+
+    return g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak, \
+        tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c
+
+
+def pituitary_ori_ode_parameters_Isk_Ibk_Ikir():
+    # Maximal conductance
+    g_CaL = np.random.uniform(0, 4)
+    g_CaT = 0
+    g_K = np.random.uniform(0., 10.)
+    g_SK = np.random.uniform(.5, 3.5)
+    g_Kir = np.random.uniform(0, 2)
+    g_BK = np.random.uniform(0, 4)
+    g_NaV = 0
+    g_A = 0
+    g_leak = np.random.uniform(0.05, 0.4)
+
+    # Other structural parameters
+    C_m = np.random.uniform(4, 12)
+    E_leak = np.random.uniform(-75, -10)
+
+    # Kinetic variables
+    tau_m = np.random.uniform(.7, 1.3)
+    tau_ht = 1
+    tau_n = np.random.uniform(20, 40)
+    tau_BK = np.random.uniform(2, 10)
+    tau_h = 1
+    tau_hNa = 1
+    k_c = np.random.uniform(0.03, 0.21)
+
+    return g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak, \
+        tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c
+
+
+def pituitary_ori_ode_parameters_Isk_Ibk_Ikir_Icat():
+    # Maximal conductance
+    g_CaL = np.random.uniform(0, 4)
+    g_CaT = np.random.uniform(0, 4)
+    g_K = np.random.uniform(0., 10.)
+    g_SK = np.random.uniform(.5, 3.5)
+    g_Kir = np.random.uniform(0, 2)
+    g_BK = np.random.uniform(0, 4)
+    g_NaV = 0
+    g_A = 0
+    g_leak = np.random.uniform(0.05, 0.4)
+
+    # Other structural parameters
+    C_m = np.random.uniform(4, 12)
+    E_leak = np.random.uniform(-75, -10)
+
+    # Kinetic variables
+    tau_m = np.random.uniform(.7, 1.3)
+    tau_ht = np.random.uniform(.7, 1.3)
+    tau_n = np.random.uniform(20, 40)
+    tau_BK = np.random.uniform(2, 10)
+    tau_h = 1
+    tau_hNa = 1
+    k_c = np.random.uniform(0.03, 0.21)
+
+    return g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak, \
+        tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c
+
+
+def pituitary_ori_ode_parameters_Isk_Ibk_Ikir_Icat_Ia():
+    # Maximal conductance
+    g_CaL = np.random.uniform(0, 4)
+    g_CaT = np.random.uniform(0, 4)
+    g_K = np.random.uniform(0., 10.)
+    g_SK = np.random.uniform(.5, 3.5)
+    g_Kir = np.random.uniform(0, 2)
+    g_BK = np.random.uniform(0, 4)
+    g_NaV = 0
+    g_A = np.random.uniform(0, 100)
+    g_leak = np.random.uniform(0.05, 0.4)
+
+    # Other structural parameters
+    C_m = np.random.uniform(4, 12)
+    E_leak = np.random.uniform(-75, -10)
+
+    # Kinetic variables
+    tau_m = np.random.uniform(.7, 1.3)
+    tau_ht = np.random.uniform(.7, 1.3)
+    tau_n = np.random.uniform(20, 40)
+    tau_BK = np.random.uniform(2, 10)
+    tau_h = np.random.uniform(10, 30)
+    tau_hNa = 1
+    k_c = np.random.uniform(0.03, 0.21)
+
+    return g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak, \
+        tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c
+
+
+def pituitary_ori_ode_parameters_Isk_Ibk_Ikir_Icat_Ia_Inav():
+    # Maximal conductance
+    g_CaL = np.random.uniform(0, 4)
+    g_CaT = np.random.uniform(0, 4)
+    g_K = np.random.uniform(0., 10.)
+    g_SK = np.random.uniform(.5, 3.5)
+    g_Kir = np.random.uniform(0, 2)
+    g_BK = np.random.uniform(0, 4)
+    g_NaV = np.random.uniform(6, 10, 16)
+    g_A = np.random.uniform(0, 100)
+    g_leak = np.random.uniform(0.05, 0.4)
+
+    # Other structural parameters
+    C_m = np.random.uniform(4, 12)
+    E_leak = np.random.uniform(-75, -10)
+
+    # Kinetic variables
+    tau_m = np.random.uniform(.7, 1.3)
+    tau_ht = np.random.uniform(.7, 1.3)
+    tau_n = np.random.uniform(20, 40)
+    tau_BK = np.random.uniform(2, 10)
+    tau_h = np.random.uniform(10, 30)
+    tau_hNa = np.random.uniform(1.4, 2.6)
+    k_c = np.random.uniform(0.03, 0.21)
+
+    return g_CaL, g_CaT, g_K, g_SK, g_Kir, g_BK, g_NaV, g_A, g_leak, C_m, E_leak, \
+        tau_m, tau_ht, tau_n, tau_BK, tau_h, tau_hNa, k_c
+
+
+def generate_pituitary(parameter_function):
+    # Initial conditions
+    V = -60.
+    n = 0.1
+    m = 0.1
+    b = 0.1
+    h = 0.01
+    h_T = 0.01
+    h_Na = 0.01
+    c = 0.1
+
+    w0 = (V, n, m, b, h, h_T, h_Na, c)
+
+    abserr = 1.0e-8
+    relerr = 1.0e-6
+
+    dt = 0.5
+    t = np.arange(0, 50000, dt)
+
+    wsol = odeint(pituitary_ode, w0, t, args=parameter_function(), atol=abserr, rtol=relerr)
+
+    return wsol, p
+
+
+def find_pituitary_activation_event(wsol_trimmed, V_threshold, dV_max_threshold, dV_min_threshold, dVs):
+    below_event_end_threshold = np.all(np.stack((wsol_trimmed[1:] < V_threshold, dVs > dV_min_threshold), axis=-1),
+                                       axis=1)
+    above_event_start_threshold = np.all(np.stack((wsol_trimmed[1:] > V_threshold, dVs > dV_max_threshold), axis=-1),
+                                         axis=1)
+    # Skip to the end of an event
+    first_event_end_index = np.argmax(below_event_end_threshold)
+
+    if first_event_end_index >= 80000:
+        return 0, 80000
+
+    # Skip to the start of the next event
+    event_start_index = np.argmax(above_event_start_threshold[first_event_end_index:]) + first_event_end_index
+
+    if event_start_index >= 80000:
+        return 0, 80000
+
+    # Skip to the end of the next event
+    event_end_index = np.argmax(below_event_end_threshold[event_start_index:]) + event_start_index
+
+    if event_end_index >= 80000:
+        return 0, 80000
+
+    return event_start_index, event_end_index
+
+
+def classify_pituitary_ode(wsol, recognise_one_burst_spiking=True):
+    """
+    Classifies the pituitary ODE as either spiking, bursting,
+    depolarised, hyperpolarised or one-spike bursting.
+
+    The classes are returned as numbers:
+    0: hyperpolarised
+    1: Depolarised
+    2: Spiking
+    3: Bursting
+    4: One-spike bursting
+
+    Arguments:
+        wsol : The sequence to classify
+        recognise_one_burst_spiking : Whether to recognise one-spike bursting
+                                      as separate from bursting.
+    """
+    min_distance = 50.
+
+    first_10_s = round(10000. / dt)
+    wsol_trimmed = wsol[first_10_s:]
+
+    highest_peak = np.max(wsol_trimmed)
+    lowest_valley = np.min(wsol_trimmed)
+
+    min_amplitude = 10.  # 20, 30?
+
+    if highest_peak > lowest_valley + min_amplitude:
+        dVs = np.diff(wsol_trimmed)
+
+        highest_dV = np.max(dVs)
+        lowest_dV = np.min(dVs)
+
+        V_threshold = (highest_peak - lowest_valley) * .35 + lowest_valley
+        dV_max_threshold = highest_dV * .25
+        dV_min_threshold = lowest_dV * .25
+
+        top_peaks = peakutils.indexes(wsol_trimmed, thres=0.8)
+        if len(top_peaks) > 1:
+            stride = max(round((top_peaks[1] - top_peaks[0]) / 50), 1)
+        else:
+            stride = 1
+
+        event_start, event_end = find_pituitary_activation_event(wsol_trimmed, V_threshold, dV_max_threshold,
+                                                                 dV_min_threshold, dVs)
+        # Error handling
+        if event_end >= 80000:
+            return 0, (20000, 21000)
+
+        min_dist = round((event_end - event_start) * 0.05)
+        if min_dist < min_distance:
+            min_dist = min_distance
+
+        nearby_peaks = peakutils.indexes(wsol_trimmed[event_start:event_end], thres=0.3, min_dist=min_dist)
+
+        if len(nearby_peaks) > 1:
+            return 3, (event_start, event_end)
+
+        else:
+            event_amplitude = np.max(wsol_trimmed[event_start:event_end]) - np.min(wsol_trimmed[event_start:event_end])
+
+            V_area = np.sum(wsol_trimmed[event_start:event_end]) - V_threshold * (event_end - event_start)
+            if V_area > 3000 / dt and event_amplitude > 30:
+                if recognise_one_burst_spiking:
+                    return 4, (event_start, event_end)
+                else:
+                    return 3, (event_start, event_end)
+            else:
+                return 2, (event_start, event_end)
+
+    else:
+        if highest_peak > -30:
+            return 1, (20000, 21000)
+        else:
+            return 0, (20000, 21000)
